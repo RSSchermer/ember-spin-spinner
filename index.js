52e0475c5b96ca34e10e8241b6b44dcb868928e2
R.S. Schermer <roland0507@gmail.com>
2015-06-12 00:37:56 +0200

    Added guard to included hook to allow using this addon as a nested addon dependency.

--- index.js
+++ index.js
@@ -12,9 +12,18 @@ module.exports = {
   included: function(app) {
     this._super.included.apply(this, arguments);
 
-    var spinPath = path.join(app.bowerDirectory, 'spin.js');
+    /* This check is a temporary hack to make it possible to use this addon
+     * both as a top level addon dependency, and as a nested addon dependency
+     * for another addon, pending resolution of https://github.com/ember-cli/ember-cli/issues/3718
+     *
+     * Any addon that wants to use this addon as a nested addon dependency needs
+     * to do the below imports itself in its included hook.
+     */
+    if (typeof(app.import) === 'function') {
+      var spinPath = path.join(app.bowerDirectory, 'spin.js');
 
-    app.import(path.join(spinPath, 'spin.js'));
-    app.import(path.join(spinPath, 'jquery.spin.js'));
+      app.import(path.join(spinPath, 'spin.js'));
+      app.import(path.join(spinPath, 'jquery.spin.js'));
+    }
   }
 };
